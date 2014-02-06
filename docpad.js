68e7c1998ec2a89b7c1d15ddce066b6759173035
Serge Stinckwich <Serge.Stinckwich@gmail.com>
2014-02-06 23:17:02 +0100

    Change title

--- docpad.js
+++ docpad.js
@@ -7,8 +7,8 @@ module.exports = {
 
     // Conference info
     conf: {
-      name: "RESSACS-14",
-      description: "Conference description",
+      name: "RESeaux de capteurS et Applications Critiques de Surveillance (RESSACS'14)",
+      description: "RESeaux de capteurS et Applications Critiques de Surveillance (RESSACS)",
       date: "3-4 Juin, 2014",
       // If your event is free, just comment this line
       //price: "$100",
