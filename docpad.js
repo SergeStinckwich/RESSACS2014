67babd0ac5ea7e9de5b6061ed45d24ce80103aa1
Serge Stinckwich <Serge.Stinckwich@gmail.com>
2014-05-27 11:02:54 +0200

    Add Mohamed talk

--- docpad.js
+++ docpad.js
@@ -62,6 +62,10 @@ module.exports = {
 
     // The entire schedule
     schedule: [
+    {
+      name: "Accueil - Café",
+      time: "3 Juin, 14h00-14h15"
+    },
       {
         name: "François Beauducel",
         photo: "themes/ird/img/francois-beauducel.jpg",
@@ -74,7 +78,7 @@ module.exports = {
         presentation: {
           title: "Le SI WEBOBS: un ensemble d’outils intégrés de gestion pour les observatoires telluriques",
           description: "Les observatoires volcanologiques et sismologiques doivent assurer une double mission de collecte de données pluridisciplinaires à des fins de recherche et une mission de surveillance temps-réel et d'alerte aux autorités. Ces objectifs entrainent la gestion de réseaux de capteurs très variés associés à des données extrêmement hétérogènes (paramètres physico-chimique, échantillonnages manuel/automatique, fréquence et formats d'acquisition, métadonnées ...). Le système WEBOBS, développé depuis 2001 et implanté dans plusieurs observatoires opérationnels, apporte une solution intégrée et effective à ces problématiques.",
-          time: ""
+          time: "3 Juin, 14h15-15h"
         }
       },
       {
@@ -89,7 +93,7 @@ module.exports = {
         presentation: {
           title: "High resolution U.A.V. thermal imagery for the study of pest dynamics in the tropical Andes",
           description: "Les drones sont de nouveaux outils très prometteurs pour la recherche. Equipés de tous types de capteurs, volant bas et lentement, ils permettent l’acquisition  rapide, bon marché et répétable de données spatialisées à très hautes résolutions. Cette présentation exposera les travaux effectués dans les Andes équatoriennes pour la caractérisation des patrons spatio-temporels d'hétérogénéité thermique des agrosystèmes à l’échelle de l’insecte ravageur. L’utilisation de caméra thermique embarquée sur un drone combinée à de nombreux capteurs au sol a permis l’obtention d’orthophotos visuelles et infrarouges de hautes résolutions (1,3 cm/px et 5 cm/px respectivement) et la modélisation du microclimat des agrosystèmes (air, plante et sol). Ces données analysées dans des SIG mettent en évidence l’effet significatif du microclimat sur la présence et distribution des insectes ravageurs dans les agro-systèmes andins.",
-          time: ""
+          time: "3 Juin, 15h-15h45"
         }
       },
       {
@@ -104,9 +108,66 @@ module.exports = {
         presentation: {
           title: "Réseaux de capteurs pour la surveillance, d'une démarche adhoc vers une approche standardisée",
           description: "Les réseaux de capteurs sont des technologies très prometteuses pour un grand nombre d'applications à fort impact sociétal et environnemental. De nombreuses infrastructures expérimentales ont été mises en place, souvent de manière adhoc en développant les outils, les protocoles et les environnements logiciels adéquats. Depuis quelques années, nous assistons à l'émergence d'un besoin générique d'adresser et de gérer un grand nombre d'objets communicants (dont les réseaux de capteurs) pour collecter et traiter de grandes masses/flux de données. Dans cette présentation, nous montrerons comment passer d'une gestion adhoc vers une gestion plus générique et standardisé de telles infrastructures pour permettre un connectivité de bout-en-bout avec le réseau Internet et bénéficier de toute la puissance de traitement des nouvelles infrastructures de type cloud.",
-          time: ""
+          time: "3 Juin, 15h45-16h30"
         }
       },
+      {
+        name: "Mohamed Babram",
+        photo: "themes/ird/img/speaker.png",
+        bio: "",
+        company: "Université Cadi Ayyad, Maroc",
+        link: {
+          href: "",
+          text: ""
+        },
+        presentation: {
+          title: "Une approche capteurs pour le contrôle et l'optimisation du trafic urbain",
+          description: "",
+          time: "3 Juin, 16h30-16h50"
+        }
+      },
+      {
+        name: "BBQ & Démonstration de drone",
+        time: "3 Juin, 19h00-21h00"
+      },
+        {
+          name: "Florent Arrignon",
+          photo:"themes/ird/img/florent_arrignon.jpg",
+          bio: "MAD-Environnement is a young innovative company based in Southern France. Its activities encompass data mining, modelling and risk analysis. MAD-Environnement brings innovative and controlled solutions to many segments and activities, such as environmental and health services, food processing, or shipbuilding industries. MAD-Environnement provides its skills via three operation modes: i) Mathematical and Risk expertise and training, ii) Model building and iii) Software development.",
+          company: "MAD-Environnement",
+          link:{
+            href: "http://mad-environnement.com/",
+            text:"Web site"
+          },
+          presentation:{
+          title: "Risque et réseaux de capteurs: potentiel d’application de la méthodologie d’Analyse Globale des Risques",
+          description: "Les réseaux de capteurs sont des systèmes complexes de par la structure des éléments les constituant, leur architecture de déploiement et leur généricité d’applications. Du fait de cette complexité, un réseau de capteurs est exposé à de nombreux risques de différentes natures (techniques, environnementaux, humains, économiques …) et à différents stades de vie du réseau (conception, déploiement, fonctionnement, démantèlement, …). Pour obtenir une vision globale des risques auxquels est exposé ce système, il est important d’éviter de les traiter par des disciplines cloisonnées et des outils trop spécifiques. L’Analyse Globale des Risques est une méthode qui permet de répondre à cette contrainte, conformément à la norme ISO 31000 de management des risques. Le potentiel d’application de cette méthode aux réseaux de capteurs est présenté par le biais d’illustrations issues d’applications sur d’autres systèmes complexes et par la spécification des besoins pour documenter une telle analyse.",
+          time: "4 Juin, 9h00-9h45"
+        }
+      },
+{
+  name: "Coffee Break",
+  time: "Mercredi 4 Juin, 9h45-10h"
+},
+{
+  name:"Hiep Xuan Huynh",
+  photo: "themes/ird/img/hiep-xuan-huynh.jpg",
+  bio:"Huỳnh Xuân Hiệp is currently associate professor in the Department of Software Engineering, College of Information and Communication Technology, Can Tho  University. He received the engineer, master and Ph.D. degrees in computer science  from Can Tho University (CTU), l’Institut de la Francophonie pour l’Informatique (IFI)  and Polytechnics School of Nantes University (Polytech’Nantes) in 1996, 1998 and 2006 respectively. His research interests includes knowledge discovery from databases, machine learning, simulation, modeling decisions, bioinformatics, embedded systems and wireless sensor network. He is also the head of the DREAM (Decision-support Research for Environmental Applications and Models) multidisciplinary research team associated with l’Institut de Recherche pour le Développement (IRD).",
+  company:"Can Tho University, Vietnam",
+  link:{
+    href:"",
+    text:""
+  },
+  presentation:{
+    title:"Mekong Delta: measuring and managing risks, a case for wireless sensor networks",
+    description:"",
+    time:"4 Juin, 10h00-10h45"
+  }
+},
+{
+  name: "Session Démonstration",
+  time: "Mercredi 4 Juin, 10h45-11h30"
+},
       {
         name:"Bernard Pottier",
         photo: "themes/ird/img/bernard-pottier.jpg",
@@ -119,29 +180,18 @@ module.exports = {
         presentation: {
           title: "Global warming, global sharing, a case for micro satellites and sensor networks",
           description: "Les capacités des capteurs et des communications sans fil permettent de construire des systèmes de perception physique des évolutions de l'environnement. La réalisation de systèmes de collectes aériens ou satellitaires peut désormais permettre la récupération et la diffusion ouverte de ces informations, parfois lointaines et inaccessibles, à l'image d'un  internet 'spatial'. Nous présentons ce contexte, et les techniques de simulation proposées pour un projet <<MIcro-satellite et réseaux de CApteurs>>.",
-          time: ""
+          time: "4 Juin, 11h30-12h45"
           }
         },
         {
-          name:"Onil Goubier",
-          photo:"themes/ird/img/onil-goubier.jpg",
-          bio:"Diplômée de l'ITB Indonésie et de l'université de Rennes I, Onil a travaillé dans les systèmes parallèles, distribués et temps réel, dans l'industrie et en académique. Elle s'intéresse actuellement aux réseaux des capteurs pour des applications de prévention des catastrophes naturelles, et sur des aspects de diffusion sociétale de capteurs.",
-          company:"Association Takanea / DRR Indonesia Foundation",
-          link:{
-            href:"",
-            text:""
-          },
-          presentation: {
-            title: "Réseaux de capteurs pour la surveillance des inondations à Jakarta",
-            description: "Jakarta est une ville très affectée par des inondations récurrentes, aggravées par les changements climatiques. Les réseaux de capteurs sont une solution prometteuse pour surveiller, alerter, collecter des données et étudier des inondations. Nous présenterons la problématique de Jakarta, l'infrastructure en place et les efforts en réseaux de capteurs.",
-            time:""
-          }
+          name: "Repas",
+          time: "4 Juin, 12h45-14h00"
         },
         {
           name:"Sébastien Chipeaux",
           photo:"themes/ird/img/sebastien-chipeaux.jpg",
           bio:"J'ai soutenu mon doctorat d'Informatique le 17 Décembre 2013. Mon sujet de thèse était la Génération automatique de systèmes multi-agents à partir de modèle pour la simulation de systèmes complexes géographiques à entités mobiles. Les travaux menés au cours de ce doctorat fournissent un méta-modèle graphique et outil de modélisation adaptés à la création de modèles agents qui représentent la dimension géographique et entités mobiles qui composent les systèmes étudiés : par exemple une ville. La simulation générée automatiquement à partir de ce modèle permet à des scientifiques non-informaticiens d'appréhender l'impact de différents scenarii sur le comportement des entités qui peuplent le système : les habitants de la ville par exemple. J'effectue actuellement un post-doctorat dans le cadre d'un projet européen sur la surveillance de machines industrielles à l'aide de réseaux de capteurs pour la maintenance prédictive.",
-          company:"",
+          company:"DISC/FEMTO-ST",
           link:{
             href:"",
             text:""
@@ -149,23 +199,16 @@ module.exports = {
           presentation: {
             title: "Réseau de capteurs sans fil pour la surveillance de machines industrielles",
             description: "La thématique réseaux de capteurs est étudiée depuis 10 ans au sein de l'équipe CARTOON du Département d'Informatique des Systèmes Complexes (DISC) du laboratoire FEMTO-ST à l'Université de Franche-Comté. Dans ce cadre nous participons au projet européen MainPreSI dans le cadre de l'INTERREG IV. Nous souhaitons utiliser une technologie innovante basée sur un ensemble capteurs passifs sans fil + interrogateur disant qui alimente, communique et interroge les capteurs disposés sur la machine à surveiller grâce à la technologie RADAR. Plusieurs verrous doivent être levés comme, par exemple, la validité des données reçues par l'interrogateur, la communication entre interrogateur ou encore le traitement des données.",
-            time:""
+            time:"4 Juin, 14h00-14h45"
           }
         },
         {
-          name:"Hiep Xuan Huynh",
-          photo: "themes/ird/img/hiep-xuan-huynh.jpg",
-          bio:"Huỳnh Xuân Hiệp is currently associate professor in the Department of Software Engineering, College of Information and Communication Technology, Can Tho  University. He received the engineer, master and Ph.D. degrees in computer science  from Can Tho University (CTU), l’Institut de la Francophonie pour l’Informatique (IFI)  and Polytechnics School of Nantes University (Polytech’Nantes) in 1996, 1998 and 2006 respectively. His research interests includes knowledge discovery from databases, machine learning, simulation, modeling decisions, bioinformatics, embedded systems and wireless sensor network. He is also the head of the DREAM (Decision-support Research for Environmental Applications and Models) multidisciplinary research team associated with l’Institut de Recherche pour le Développement (IRD).",
-          company:"Can Tho University, Vietnam",
-          link:{
-            href:"",
-            text:""
-          },
-          presentation:{
-            title:"Mekong Delta: measuring and managing risks, a case for wireless sensor networks",
-            description:"",
-            time:""
-          }
+          name: "Coffee Break",
+          time: "4 Juin, 14h45-15h00"
+        },
+        {
+          name: "Session Démonstration",
+          time: "4 Juin, 15h00-15h45"
         },
         {
           name:"Stéphane Louise",
@@ -179,23 +222,23 @@ module.exports = {
           presentation:{
           title:"A Model of Computation for highly parallel and dynamic system",
           description:"Todays challenges in real-time systems including sensor networks is that they have massive scale with high parallelism level, and at the same time they can be highly dynamic, with parts that are intermittently connected to a core system. For fixed (non dynamic) systems, dataflow paradigms works reasonably well, especially for signal processing. But with parts of the system that connect and disconnect in a unpredictable fashion, the usual dataflow models does not work. We want to show how to improve a deterministic dataflow model (mostly CSDF) with data management which retains the deterministic properties of the base modelbut without the constraints of a static (non dynamic) system.",
-          time:""
+          time:"4 Juin, 15h45-16h30"
         }
       },
-        {
-          name: "Florent Arrignon",
-          photo:"themes/ird/img/florent_arrignon.jpg",
-          bio: "MAD-Environnement is a young innovative company based in Southern France. Its activities encompass data mining, modelling and risk analysis. MAD-Environnement brings innovative and controlled solutions to many segments and activities, such as environmental and health services, food processing, or shipbuilding industries. MAD-Environnement provides its skills via three operation modes: i) Mathematical and Risk expertise and training, ii) Model building and iii)Software development.",
-          company: "MAD Environnement",
-          link:{
-            href: "http://mad-environnement.com/",
-            text:"Web site"
-          },
-          presentation:{
-          title: "Risque et réseaux de capteurs: potentiel d’application de la méthodologie d’Analyse Globale des Risques",
-          description: "Les réseaux de capteurs sont des systèmes complexes de par la structure des éléments les constituant, leur architecture de déploiement et leur généricité d’applications. Du fait de cette complexité, un réseau de capteurs est exposé à de nombreux risques de différentes natures (techniques, environnementaux, humains, économiques …) et à différents stades de vie du réseau (conception, déploiement, fonctionnement, démantèlement, …). Pour obtenir une vision globale des risques auxquels est exposé ce système, il est important d’éviter de les traiter par des disciplines cloisonnées et des outils trop spécifiques. L’Analyse Globale des Risques est une méthode qui permet de répondre à cette contrainte, conformément à la norme ISO 31000 de management des risques. Le potentiel d’application de cette méthode aux réseaux de capteurs est présenté par le biais d’illustrations issues d’applications sur d’autres systèmes complexes et par la spécification des besoins pour documenter une telle analyse.",
-          time: ""
-        }
+      {
+        name:"Onil Goubier",
+  photo:"themes/ird/img/onil-goubier.jpg",
+  bio:"Diplômée de l'ITB Indonésie et de l'université de Rennes I, Onil a travaillé dans les systèmes parallèles, distribués et temps réel, dans l'industrie et en académique. Elle s'intéresse actuellement aux réseaux des capteurs pour des applications de prévention des catastrophes naturelles, et sur des aspects de diffusion sociétale de capteurs.",
+  company:"Association Takanea / DRR Indonesia Foundation",
+  link:{
+    href:"",
+    text:""
+  },
+  presentation: {
+    title: "Réseaux de capteurs pour la surveillance des inondations à Jakarta",
+    description: "Jakarta est une ville très affectée par des inondations récurrentes, aggravées par les changements climatiques. Les réseaux de capteurs sont une solution prometteuse pour surveiller, alerter, collecter des données et étudier des inondations. Nous présenterons la problématique de Jakarta, l'infrastructure en place et les efforts en réseaux de capteurs.",
+    time: "4 Juin, 16h30-17h15"
+  }
         }
       // {
       //   name: "Steve Jobs",
@@ -275,6 +318,11 @@ module.exports = {
         name: "DSI IRD",
         logo: "themes/ird/img/DSI-logo.png",
         url: "https://www.ird.fr/dsi/"
+      },
+      {
+        name: "LIUPPA/T2I",
+        logo: "themes/ird/img/LIUPPA-T2I.jpg",
+        url: "http://liuppa.univ-pau.fr/"
       }
     ],
 
